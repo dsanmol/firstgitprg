2a415b31e1225e2cf985c0abbcc870e429385488
dsanmol <143399771+dsanmol@users.noreply.github.com>
2023-09-12 10:36:34 +0530

    Add files via upload

---
+++ Stringswitch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Stringsw {
+    public static void main(String[] args) {
+        String a="Anmol  i ";
+        System.out.println(a);
+        /*
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter Your Age");
+
+        int age = a.nextInt();
+         
+        //switch (age) {
+          //  case 18:
+            //    System.out.println("You Are Eligible To Vote!!!");
+              //  break;
+        
+            //case 17:
+              //  System.out.println("You Are Not Eligible To Vote!!!");
+            
+                //break;
+        //}
+        if (age>18) {
+            System.out.println("You Are Eligible To Vote!!!");
+        }
+        if (age<18) {
+            System.out.println("You Are Not Eligible To Vote!!!");
+        }
+        */
+        String q ="m";
+        int b = a.indexOf(q);
+        System.out.println(b);
+
+
+    }
+    
+}
