5137365b771de58692eb9ba5c4b35a4518557419
Anmol Gautam <anmol100gautam+github@gmail.com>
2024-02-24 16:48:22 +0530

    Add files via upload

---
+++ Python projects/firstpythonprg.py
@@ -0,0 +1,18 @@
+a=int(input("Enter number 1"))
+b=int(input("Enter number 2"))
+c=input("Enter what operation to perform")
+if(c=='+'):
+    d=a+b
+    print(d)
+if(c=='-'):
+    d=a-b
+    print(d)
+
+if(c=='*'):
+    d=a*b
+    print(d)
+
+
+if(c=='/'):
+    d=a/b
+    print(d)
---
+++ Python projects/strings.py
@@ -0,0 +1,5 @@
+a=input("Enter your name")
+b=a(2)
+print(b)
+for i in range(0,len(a)):
+    print(a[i],i)
